500ea835674960368a6bbd87737157bc59f1cf2d
Tobias Behr <15959166+TobiasB90@users.noreply.github.com>
2018-05-02 13:56:36 +0200

    Update complete project / Add testing scene

--- Endless_Tron_Flyer/Assets/_Scripts/_Player/CameraController.cs
+++ Endless_Tron_Flyer/Assets/_Scripts/_Player/CameraController.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Camera MainCamera;
+    public bool RotationWithCam;
+    public Transform target;
+    public Vector3 offset;
+    public float pitch = 2f;
+    public float currentYaw = 0f;
+    Quaternion baseRota;
+    public float multiplier;
+
+
+    private void Start()
+    {
+        if (MainCamera == null)
+        {
+            MainCamera = Camera.main;
+        }
+        baseRota = MainCamera.transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        MainCamera.transform.position = target.position - offset;
+        MainCamera.transform.LookAt(target.position + Vector3.up * pitch);
+        MainCamera.transform.RotateAround(target.position, Vector3.up, currentYaw);
+        if (RotationWithCam)
+        {
+            MainCamera.transform.rotation = Quaternion.Euler(baseRota.x, baseRota.y, target.rotation.z * multiplier);
+            // MainCamera.transform.rotation = new Quaternion(baseRota.x, baseRota.y, target.rotation.z, baseRota.w);
+
+        }
+    }
 }
--- Endless_Tron_Flyer/Assets/_Scripts/_Player/PlayerController.cs
+++ Endless_Tron_Flyer/Assets/_Scripts/_Player/PlayerController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float flyingspeed;
+    public float rotationspeed;
+    public bool invertedmovement = true;
+    public bool backToDefPos;
+    public float currentVelocity;
+
+    public float Vert;
+    public float Horz;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vert = Input.GetAxis("Vertical");
+        Horz = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(Input.GetAxis("Vertical") * rotationspeed, 0.0f, -Input.GetAxis("Horizontal") * rotationspeed);
+        }
+        else transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, Mathf.SmoothDamp(transform.rotation.z, 0, ref currentVelocity, 1), transform.rotation.w);
+        // player, destroyer and builder flying forward at the speed of 'flyingspeed'
+        float step = flyingspeed * Time.deltaTime;
+        transform.position += transform.forward * step;
+        // EndlessBuilderDestroyer.transform.position = new Vector3(0, 0, transform.position.z);
+    }
 }
