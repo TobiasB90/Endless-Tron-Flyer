33b386960bb0f10848254ca38ad289bf497eabe4
Tobias Behr <15959166+TobiasB90@users.noreply.github.com>
2018-05-02 14:56:30 +0200

    New camera & playercontroller

--- Endless_Tron_Flyer/Assets/_Scripts/_Player/CameraController.cs
+++ Endless_Tron_Flyer/Assets/_Scripts/_Player/CameraController.cs
@@ -26,9 +26,11 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        MainCamera.transform.position = target.position - offset;
-        MainCamera.transform.LookAt(target.position + Vector3.up * pitch);
-        MainCamera.transform.RotateAround(target.position, Vector3.up, currentYaw);
+        Vector3 moveCamTo = target.position - transform.forward * 10f + Vector3.up * 2.0f;
+        float bias = 0.96f;
+        MainCamera.transform.position = MainCamera.transform.position * bias + moveCamTo * (1.0f-bias);
+        MainCamera.transform.LookAt(target.position + target.forward * 5);
+        // MainCamera.transform.RotateAround(target.position, Vector3.up, currentYaw);
         if (RotationWithCam)
         {
             MainCamera.transform.rotation = Quaternion.Euler(baseRota.x, baseRota.y, target.rotation.z * multiplier);
