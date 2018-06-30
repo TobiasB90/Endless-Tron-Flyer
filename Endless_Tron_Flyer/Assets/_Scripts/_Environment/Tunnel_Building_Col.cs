2a9f3cc0ac954b1817f33aff2222bacede745be1
Tobias Behr <15959166+TobiasB90@users.noreply.github.com>
2018-06-30 13:40:29 +0200

    Add Tunnel/Building collision

---
+++ Endless_Tron_Flyer/Assets/_Scripts/_Environment/Tunnel_Building_Col.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Tunnel_Building_Col : MonoBehaviour {
+
+    public LayerMask BuildingLayer;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        if ((BuildingLayer & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
